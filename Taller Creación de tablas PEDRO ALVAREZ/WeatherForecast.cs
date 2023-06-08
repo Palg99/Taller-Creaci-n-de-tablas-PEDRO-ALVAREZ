f6440f3bdb57e27e0bb0cbb39b43710c537cdd26
palg99 <alvarezguashpapedroisrael@gmail.com>
2023-06-08 00:04:16 -0500

    Agregar archivos de proyecto.

---
+++ Taller Creación de tablas PEDRO ALVAREZ/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Taller_Creación_de_tablas_PEDRO_ALVAREZ
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
